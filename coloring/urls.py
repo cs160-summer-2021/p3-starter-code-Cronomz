e3fb4e2868cc620bf18f374c551905cbe76632c9
Connor Lin <connorlin@berkeley.edu>
2021-07-12 21:16:05 -0700

    implemented save/share page

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('save_share', views.save_share, name='save_share'),
+    path('help', views.help, name='help')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+def save_share(request):
+    return render(request, 'coloring/save_share.html')
+
+def help(request):
+    return render(request, 'coloring/help.html')
